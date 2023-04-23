d09d0aac3550da511862a0bf1d74cee0c09d285f
yuvalEG <egoziyuval@gmail.com>
2023-04-23 07:18:20 +0300

    New version - added features

--- UI.py
+++ UI.py
@@ -1,21 +1,31 @@
 import tkinter as tk
 import chordGen
 
-# basic settings
+# Basic settings
 root = tk.Tk()
 root.title("Random Chord Generator")
-root.geometry("450x150")
+root.geometry("450x175")
+
+# label to present the chord
+output_label = tk.Label(root, text="", font='Arial 75')
+output_label.pack()
+
+# 'Use Seventh' checkbox
+seventh_checker = tk.IntVar()
+seventh_checkbox = tk.Checkbutton(root, text='Use Seventh Chords', variable=seventh_checker)
+seventh_checkbox.pack(anchor='center', padx= 5)
+
+# 'Simplify' checkbox
+simplify_checker = tk.IntVar()
+simplify_checkbox = tk.Checkbutton(root, text='Simplify Chords', variable=simplify_checker)
+simplify_checkbox.pack(anchor='center', padx= 5)
 
 
 def run_program():
-    label_text = chordGen.random_simple_chord()
+    label_text = chordGen.build_chord(simplify_checker.get(), seventh_checker.get())
     output_label.config(text=label_text)
 
 
-# creating a button
-button = tk.Button(root, text="Run", font= 'Helvetica 20', command=run_program)
+# creating a 'Run' button
+button = tk.Button(root, text="Run", font='Helvetica 20', command=run_program)
 button.pack(pady=5)
-
-# creating a chord label
-output_label = tk.Label(root, text="", font='Arial 75')
-output_label.pack()
--- chordGen.py
+++ chordGen.py
@@ -1,51 +1,92 @@
 import random
 
+# Building blocks of the most common chord notations
 SimpleChordNames = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'F', 'F#', 'G', 'Ab']
-SimpleChordTypes = ['Dim', 'm', '', '+', 'Dim7-', '7b5', 'm7', 'mMaj7', '7', 'Maj7']
+# TODO in case of change in SimpleChordTypes check function build_chord
+SimpleChordTypes = ['Dim', 'm', '']
+SimpleSeventhChordTypes = ['7b5', 'm7', '7', 'Maj7']
+SimpleDimSeventh = ['m7', '7']
+SimpleMinorSeventh = ['7b5', '7']
 
-
-def random_simple_chord():
-    random_simple_chord_number = random.randint(0, len(SimpleChordNames) - 1)
-    random_simple_chord_type_number = random.randint(0, len(SimpleChordTypes) - 1)
-    return SimpleChordNames[random_simple_chord_number] \
-        + SimpleChordTypes[random_simple_chord_type_number]
-
-
-# Building chord name basic blocks
+# Building every possible chord name basic blocks
 ChordLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 Accidentals = ['', 'b', '#']
-EvilAccidentals = ['', 'b', 'bb', '#', '##']
+# Building blocks of the  most common chord types
+ChordTypes = ['Dim', 'm', '', '+']
+SeventhChordTypes = ['7-', '7', 'Maj7']
 
-# Chord Name lists - one normal and one evil
-EnharmonicChords = []
+"""
+There should be another list with chords other than 7ths - 6, 9b, sus2, etc.
+Maybe even other basic chord types - like quartal
+"""
+
+ChordNames = []
 for ChordLetter in ChordLetters:
-    for accident in Accidentals:
-        EnharmonicChord = ChordLetter + accident
-        EnharmonicChords.append(EnharmonicChord)
-
-EvilEnharmonicChords = []
+    for Accident in Accidentals:
+        EnharmonicChord = ChordLetter + Accident
+        # remove 4 secretly evil accidentals
+        if EnharmonicChord != 'B#' or 'Cb' or 'E#' or 'Fb':
+            ChordNames.append(EnharmonicChord)
+'''
+# Building a list with Double Accidentals
+EvilAccidentals = ['', 'b', 'bb', '#', '##']
+EvilEnharmonicChordNames = []
 for ChordLetter in ChordLetters:
     for EvilAccident in EvilAccidentals:
         EvilEnharmonicChord = ChordLetter + EvilAccident
-        EvilEnharmonicChords.append(EvilEnharmonicChord)
-
-ChordTypes = ['Dim', 'm', '', '+']
-SeptaChordTypes = ['7-', '7', 'Maj7', '']
-# I placed the empty string in SeptaChordSigns in order to generate non-septachords, it's not best practice
-# There should be another list with chords other than 7ths - 6, 9b, sus2, etc.
-# Maybe even other chord types - like quartal
-
-Used_ChordNames = EnharmonicChords
-Used_ChordTypes = ChordTypes
-Used_SeptaChordTypes = SeptaChordTypes
+        EvilEnharmonicChordNames.append(EvilEnharmonicChord)
+'''
 
 
-def random_chord():
-    random_chord_name_selected = random.randint(0, len(Used_ChordNames) - 1)
-    random_chord_type_selected = random.randint(0, len(Used_ChordTypes) - 1)
-    random_septachord_name_selected = random.randint(0, len(Used_SeptaChordTypes) - 1)
+def get_random_element(array):
+    selected_random_value = random.randint(0, len(array) - 1)
+    return array[selected_random_value]
 
-    output = Used_ChordNames[random_chord_name_selected] \
-        + Used_ChordTypes[random_chord_type_selected] \
-        + SeptaChordTypes[random_septachord_name_selected]
-    return output
+
+def build_chord(isSimple:bool, isSeventh:bool):
+    """
+    :param isSimple:
+    :param isSeventh:
+    :return: chord based on user preference
+    IMPORTANT!
+    Compares against values in SimpleChordTypes as string
+    Do not change 'Dim' and 'm'
+    """
+
+    chordName = ""
+    chordType = ""
+    chordSeventh = ""
+
+    if isSimple:
+        chordName = get_random_element(SimpleChordNames)
+        chordType = get_random_element(SimpleChordTypes)
+
+    else:
+        chordName = get_random_element(ChordNames)
+        chordType = get_random_element(ChordTypes)
+
+
+    if isSeventh and isSimple:
+        match chordType:
+            case 'Dim':
+                chordSeventh = get_random_element(SimpleDimSeventh)
+            case 'm':
+                chordSeventh = get_random_element(SimpleMinorSeventh)
+            case _:
+                chordSeventh = get_random_element(SimpleSeventhChordTypes)
+
+
+    if isSeventh and not isSimple:
+        chordSeventh = get_random_element(SeventhChordTypes)
+
+    return chordName+chordType+chordSeventh
+
+
+'''
+    if isSeventh:
+        if simplify_checker.get():
+            used_seventh_chords = chordGen.SimpleSeventhChordTypes
+        else:
+            used_seventh_chords = chordGen.SeventhChordTypes
+'''
+
